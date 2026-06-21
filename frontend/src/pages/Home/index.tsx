f5b1873bfc69b5c7783dc9a4bbc11934c580fe29
hoanganh-ktpm <lehoanganh18102k6@gmail.com>
2026-06-22 02:33:23 +0700

    update to run frontend and backend with 1 command

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from 'react';
+import { Button } from '~/components/ui/button';
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '~/components/ui/hover-card';
+import { Link } from 'react-router-dom';
 import CourseCard from '~/components/CourseCard';
 function Home() {
     const [courses, setCourses] = useState([]);
@@ -10,9 +13,8 @@ function Home() {
             });
     }, []);
 
-
     return (
-        <div className="grid gap-6 grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid gap-6 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 relative">
             {courses.map((course, index) => {
                 return (
                     <CourseCard
@@ -24,6 +26,24 @@ function Home() {
                     />
                 );
             })}
+
+            <HoverCard openDelay={10} closeDelay={100}>
+                <HoverCardTrigger asChild>
+                    <Link to={'/courses/create'} className="flex justify-center items-center fixed bottom-10 right-10">
+                        <Button className="w-14 h-14 rounded-full text-2xl shadow-xl hover:scale-110 hover:-translate-y-2 transition-all">
+                            +
+                        </Button>
+                    </Link>
+                </HoverCardTrigger>
+                <HoverCardContent
+                side='left'
+                sideOffset={15}
+                className="flex w-64 flex-col gap-0.5">
+                    <div className="font-semibold">Add New Course</div>
+                    <div>Click here to add new course!!!!</div>
+                    <div className="mt-1 text-xs text-muted-foreground">created June 2026</div>
+                </HoverCardContent>
+            </HoverCard>
         </div>
     );
 }
